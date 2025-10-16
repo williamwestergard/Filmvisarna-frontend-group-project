6e6d4c0155a9bd072a867f62c347021e74b7625f
Daniel Arvebäck <daniel.arveback03@gmail.com>
2025-10-16 13:35:10 +0200

    moved Navbar files to Navbar folder.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
+// Fil: App.tsx
 
 import "./App.css"
 import { Routes, Route } from "react-router-dom";
+import Navbar from "../Navbar";
 import Home from "./pages/home";
 import About from "./pages/about-us";
 import BookingPage from "./pages/booking";
@@ -10,17 +12,21 @@ import TicketPage from "./pages/ticket";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about-us" element={<About />} />
-      <Route path="/shop" element={<Shop />} />
-
-
-      <Route path="/booking-page" element={<BookingPage />} />
-        <Route path="/confirmation-page" element={<ConfirmationPage />} />
-          <Route path="/ticket" element={<TicketPage />} />
+    
+    <>
+      <Navbar /> 
       
-    </Routes>
+      <main> 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about-us" element={<About />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/booking-page" element={<BookingPage />} />
+          <Route path="/confirmation-page" element={<ConfirmationPage />} />
+          <Route path="/ticket" element={<TicketPage />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
