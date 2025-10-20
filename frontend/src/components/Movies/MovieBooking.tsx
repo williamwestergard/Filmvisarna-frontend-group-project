4e04b734602d22545073177f2bd07e84bd73e369
Daniel Norén <danneazum@gmail.com>
2025-10-20 14:38:35 +0200

    changed to lowercase
    fixed the typing.

--- frontend/src/components/Movies/MovieBooking.tsx
+++ frontend/src/components/Movies/MovieBooking.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { getMovies, getMoviesInformation} from "../../api/MoviesApi";
+import { getMovies, getMoviesInformation} from "../../api/moviesApi";
 
 
 
