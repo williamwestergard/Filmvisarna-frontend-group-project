571cf3aa09d4a3947ad73b4f2ce97a89cd71e166
williamwestergard <williamwestergardd@gmail.com>
2025-10-21 23:14:39 +0200

    Minor design fixes
    Added a auditorium screen png.
    Fixed the content css and added the information at the bottom of each component

--- frontend/src/components/Auditorium/AuditoriumOne.tsx
+++ frontend/src/components/Auditorium/AuditoriumOne.tsx
@@ -1,15 +1,17 @@
 import "./auditorium.css"
+import AuditoriumScreen from "../../assets/images/auditorium/auditorium-screen.png"
 
 function Auditorium() {
 
   return (
     <>
     <section className="auditorium-content">
-      <p className="auditorium-text">Salong Helan</p>
+      <p className="auditorium-text">Salong - Helan</p>
       <h2>Välj platser</h2>
       <section className="auditorium-container">
        
 <article className="auditorium">
+  <img  className="auditorium-screen" src={AuditoriumScreen} alt="Bild på en bioduk" />
 <section className="auditorium-seats-container">
 
 <section className="auditorium-one-seats-first-row">
@@ -81,7 +83,14 @@ function Auditorium() {
 
       </section>
       <article className="auditorium-information-bottom">
-        <p>Ditt val</p><p>Upptagen</p>
+          <article className="auditorium-information-bottom-user-seat-container">
+        <article className="auditorium-information-bottom-user-seat"></article>
+        <p>Ditt val</p>
+        </article>
+          <article className="auditorium-information-bottom-occupied-seatcontainer">
+           <article className="auditorium-information-bottom-occupied-seat"></article>
+        <p>Upptagen</p>
+      </article>
       </article>
        
 </section>
