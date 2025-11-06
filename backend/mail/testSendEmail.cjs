f777e17d6698da51cd4eb9f1d2935b0563dfe6b3
williamwestergard <williamwestergardd@gmail.com>
2025-11-06 11:19:01 +0100

    initial commit
    Removed test email code.

--- backend/mail/testSendEmail.cjs
+++
@@ -1,8 +0,0 @@
-const sendEmail = require("./sendEmail.cjs");
-
-sendEmail({
-  to: "filmvisarnaabc@gmail.com",
-  subject: "Hej! Detta kommer från William!",
-  text: "Scooby dooby do",
-  html: "<p>Dooby dooby doo</p>",
-});
