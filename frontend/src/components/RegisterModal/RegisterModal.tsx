9eb1f872d5bedd0399ed8c12222e24287880ec8a
JuliaRymem <juliakrasmusson@hotmail.com>
2025-11-11 11:09:01 +0100

    add new files

---
+++ frontend/src/components/RegisterModal/RegisterModal.tsx
@@ -0,0 +1,6 @@
+// RegisterModal.tsx
+import { useEffect, useRef } from "react";
+import "./RegisterModal.css"; // optional: or reuse LoginModal.css if identical
+import RegisterForm from "../RegisterForm/RegisterForm";
+
+}
