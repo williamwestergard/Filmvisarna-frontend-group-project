8ec8ff0fd2a43f102cda0695e59b398555446aa5
JuliaRymem <juliakrasmusson@hotmail.com>
2025-11-13 09:45:21 +0100

    Add CookiePolicy page

---
+++ frontend/src/pages/CookiePolicy/CookiePolicy.tsx
@@ -0,0 +1,18 @@
+import "./CookiePolicy.css";
+          <p>
+            Framöver vill vi även kunna använda statistikcookies för att förstå hur
+            vår webbplats används. Statistiken kan i nästa steg användas som underlag
+            för marknadsföring. Du ska alltid kunna välja bort dessa.
+          </p>
+          <p>
+            Just nu använder vi inte några statistik- eller marknadsföringscookies.
+            När vi börjar göra det kommer vi:
+          </p>
+          <p>
+            Du kan alltid välja att endast använda nödvändiga cookies. I vår cookie-banner
+            kan du välja mellan att acceptera alla cookies eller endast tekniskt nödvändiga.
+          </p>
+          <p>
+            Om du har frågor om vår hantering av cookies kan du kontakta oss via
+            uppgifterna på sidan <em>Om oss</em>.
+          </p>
