15b3703befac470f2ca6946faade767a888b5ff3
JuliaRymem <juliakrasmusson@hotmail.com>
2025-11-14 09:35:23 +0100

    update "Hitta plats" to "Detaljerad vy"

--- frontend/src/components/Auditorium/Auditorium.tsx
+++ frontend/src/components/Auditorium/Auditorium.tsx
@@ -197,7 +197,7 @@ export default function Auditorium() {
           >
             <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0016 9.5 6.5 6.5 0 109.5 16a6.471 6.471 0 004.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zM10 14a4 4 0 110-8 4 4 0 010 8z" />
           </svg>
-          <span>Hitta plats</span>
+          <span>Detaljerad vy</span>
         </button>
 
         {pickerOpen && (
